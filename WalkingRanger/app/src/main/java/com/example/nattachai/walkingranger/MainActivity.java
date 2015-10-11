ef2dfb41495254c4a8f3bf805b8ef7920015f7be
Nattachai Suteerapongpan <nattachai_z@hotmail.com>
2015-10-11 18:25:50 +0700

    11.Oct.2015_18.25

--- WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
+++ WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.facebook.FacebookSdk
+import com.facebook.FacebookSdk;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {

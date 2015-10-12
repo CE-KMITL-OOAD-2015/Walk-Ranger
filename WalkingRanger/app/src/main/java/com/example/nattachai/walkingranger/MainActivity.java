2d61318a5a8a080a67117adf562698265dede336
Nattachai Suteerapongpan <nattachai_z@hotmail.com>
2015-10-12 23:53:55 +0700

    NoobLogin55555

--- WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
+++ WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
@@ -1,39 +1,57 @@
 package com.example.nattachai.walkingranger;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.AccessToken;
+import com.facebook.FacebookCallback;
+import com.facebook.CallbackManager;
+import com.facebook.login.LoginResult;
+import com.facebook.ProfileTracker;
+import com.facebook.login.widget.ProfilePictureView;
+
 public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    ProfileTracker profileTracker;
+
+    private TextView userName;
+    private ProfilePictureView profilePicture;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        // Initialize the SDK before executing any other operations,
-        // especially, if you're using Facebook UI elements.
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }

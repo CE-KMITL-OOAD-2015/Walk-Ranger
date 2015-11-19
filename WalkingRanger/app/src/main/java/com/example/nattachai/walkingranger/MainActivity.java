a821db4359ad7d9db1aa3153b10195f9dccebcfa
Chinasit316 <neat5237@hotmail.com>
2015-11-19 23:54:29 +0700

    OOAD-walkingranger

--- WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
+++ WalkingRanger/app/src/main/java/com/example/nattachai/walkingranger/MainActivity.java
@@ -1,57 +1,315 @@
 package com.example.nattachai.walkingranger;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.facebook.AccessToken;
-import com.facebook.FacebookCallback;
-import com.facebook.CallbackManager;
-import com.facebook.login.LoginResult;
+import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.widget.ProfilePictureView;
+import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity {
-    CallbackManager callbackManager;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    boolean activityRunning;
+    private String hasGroup = new String();
+    private int svcount;
+    private String isLearder = new String();
+
     ProfileTracker profileTracker;
-
     private TextView userName;
+    private TextView kak;
     private ProfilePictureView profilePicture;
+    private Member user = new Member();
+    private int nowCount = -1;
+    private Profile profile;
+
+    private ProgressBar cp1;
+    private ProgressBar cp2;
+    private ProgressBar cp3;
+    private int step;
+    private TextView count1;
+    private TextView count2;
+    private TextView count3;
+    String cc;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(this.getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-
-            }
-        });
         setContentView(R.layout.activity_main);
+
+        Intent intent =getIntent();
+        cc = intent.getStringExtra("step");
+        cp1 = (ProgressBar)findViewById(R.id.progressBar);
+        cp2 = (ProgressBar)findViewById(R.id.progressBark);
+        cp3 = (ProgressBar)findViewById(R.id.progressBarm);
+
+        count1 = (TextView) findViewById(R.id.count1);
+        count2 = (TextView) findViewById(R.id.count2);
+        count3 = (TextView) findViewById(R.id.count3);
+      //  Bundle extra = getIntent().getExtras();
+      //  String faceid = extra.getString("ID");
+
+
+        profile = Profile.getCurrentProfile();
+        //new SimpleTaskL().execute("http://203.151.92.179:8080/fblogin?name=" + profile.getName() + "&fb-code=" + profile.getId());
+        new SimpleTask().execute("http://203.151.92.179:8080/getuserprofile?id=" + MemberStatic.getFbID());
+        //while(user.getStepCount() == 0 );
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View nav_header = LayoutInflater.from(this).inflate(R.layout.nav_header_main, null);
+
+        ((TextView)nav_header.findViewById(R.id.data_ffffffname)).setText(profile.getName());
+        ((ProfilePictureView)nav_header.findViewById(R.id.profile_picture)).setProfileId(profile.getId());
+        navigationView.addHeaderView(nav_header);
+
+
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //count.setText(String.valueOf(user.getStepCount()));
+
+
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_userpro) {
+            Intent i = new Intent(getApplicationContext(),UserActivity.class);
+            startActivity(i);
+            finish();
+        } else if (id == R.id.nav_group) {
+            if(hasGroup != null) {
+                if(isLearder.equals("Leader")) {
+                    Intent i = new Intent(getApplicationContext(), LeagroupActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Intent i = new Intent(getApplicationContext(), GroupActivity.class);
+                    startActivity(i);
+                }
+                finish();
+            }
+            else {
+                Intent i = new Intent(getApplicationContext(), BgroupActivity.class);
+                startActivity(i);
+                finish();
+            }
+        } else if (id == R.id.nav_board) {
+            Intent i = new Intent(getApplicationContext(),BoardActivity.class);
+            startActivity(i);
+            finish();
+
+        } else if (id == R.id.nav_scarvenger) {
+            Intent i = new Intent(getApplicationContext(),ScarActivity.class);
+            startActivity(i);
+            finish();
+
+        } else if (id == R.id.nav_info) {
+
+
+        } else if (id == R.id.nav_logout) {
+            AlertDialog.Builder dDialog = new AlertDialog.Builder(MainActivity.this);
+            dDialog.setIcon(R.drawable.logout_exit);
+            dDialog.setTitle("Logout");
+            dDialog.setMessage("Are you sure you want to logout?");
+            dDialog.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            dDialog.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                }
+            });
+            dDialog.show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (activityRunning) {
+            if(nowCount == -1)
+                nowCount = (int)(event.values[0]);
+            int x = (int)(event.values[0])- nowCount+user.getStepCount();
+            nowCount = (int) (event.values[0]);
+            user.setStepCount(x);
+
+            new SimpleTask().execute("http://203.151.92.179:8080/update-walking-count?id=" + MemberStatic.getFbID() + "&stepCount=" + x);
+
+
+            updateCount();
+
+
+        }
+
+    }
+
+    private void updateCount() {
+        step = user.getStepCount();
+        int step1 = step%100;
+        int step2 = (step/100)%100;
+        int step3 = (step/10000)%100;
+        count1.setText(String.valueOf(step1));
+        count2.setText(String.valueOf(step2));
+        count3.setText(String.valueOf(step3));
+        cp1.setProgress(step1);
+        cp2.setProgress(step2);
+        cp3.setProgress(step3);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activityRunning = false;
+        // if you unregister the last listener, the hardware will stop detecting step events
+        // sensorManager.unregisterListener(this);
+    }
+
+    private class SimpleTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            // Create Show ProgressBar
+        }
+
+        protected String doInBackground(String... urls) {
+            String result = "";
+            IOException ee;
+            Exception ex;
+            try {
+
+                HttpGet httpGet = new HttpGet(urls[0]);
+                HttpClient client = new DefaultHttpClient();
+
+                HttpResponse response = client.execute(httpGet);
+
+                int statusCode = response.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    InputStream inputStream = response.getEntity().getContent();
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result += line;
+                    }
+                }
+
+            } catch (ClientProtocolException e) {
+
+            } catch (IOException e) {
+                ee=e;
+            }catch (Exception e){
+                ex=e;
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String jsonString) {
+            // Dismiss ProgressBar
+            showData(jsonString);
+        }
+    }
+
+    private void showData(String jsonString) {
+
+        Gson gson = new Gson();
+        user = gson.fromJson(jsonString, Member.class);
+        updateCount();
+        hasGroup = user.getGroupName();
+        isLearder = user.getRole();
+
+    }
+
+
 }
+
